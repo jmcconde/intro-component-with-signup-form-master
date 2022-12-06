9be4f1c4dc87bfa1220bf57aa4168716dabd6544
João Conde <joao997conde@gmail.com>
2022-12-06 16:31:32 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputs = document.querySelectorAll('.form-input');
+const form = document.querySelector('.form');
+
+inputs.forEach(input => {
+    input.addEventListener('input', () => {
+        if(input.value !== '') {
+            input.nextElementSibling.classList.add('float');
+        } else {
+            input.nextElementSibling.classList.remove('float');
+        }
+    })
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    inputs.forEach(input => {
+        let label = input.nextElementSibling.innerHTML;
+        let errorMsg = input.nextElementSibling.nextElementSibling;
+        if(input.value === '') {
+            input.classList.add('error');
+            input.classList.remove('valid');
+            errorMsg.innerHTML = label + " cannot be empty";
+            input.parentElement.querySelector('.error-icon').classList.remove('hidden');
+        } else {
+            input.classList.remove('error');
+            input.classList.add('valid');
+            errorMsg.innerHTML = '';
+            input.parentElement.querySelector('.error-icon').classList.add('hidden');
+            if(input.type === 'email' && !validateEmail(input.value)) {
+                input.classList.add('error');
+                input.classList.remove('valid');
+                errorMsg.innerHTML = "Looks like this is not an email";
+                input.parentElement.querySelector('.error-icon').classList.remove('hidden');
+            }
+        }
+    });
+})
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
